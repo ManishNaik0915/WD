f8ad9e4fd726a553696e7b2890eb8bfb90aab15c
ManishNaik0915 <manishnaik35980@gmail.com>
2026-08-31 11:50:28 +0530

    Day 12 JS

---
+++ JS/data.js
@@ -0,0 +1,38 @@
+//Day 12
+
+//String
+let a="Hello"
+console.log(a)
+console.log(typeof(a))
+
+let b=a.charAt(3)
+console.log(b);
+
+let c=a.charCodeAt(3)
+console.log(c);
+
+let d=a.startsWith("E")
+console.log(d);
+
+let e=a.endsWith("0")
+console.log(e);
+
+let f=a.includes("E")
+console.log(f);
+
+let g=a.toLocaleUpperCase()
+console.log(g);
+
+let h=a.toLocaleLowerCase()
+console.log(h);
+
+let i=a.concat("Fortune")
+console.log(i);
+
+let j=a.repeat(10)
+console.log(j);
+
+let k=a.slice(3)
+console.log(k);
+
+
---
+++ JS/index.js
@@ -0,0 +1,23 @@
+//Day 12
+
+// //Output methdods
+// console.log("Hello JS")
+// confirm("I am Confirm")
+// alert("I am alert")
+// document.writeln("I am Document")
+// // document.writeln("I am Document")
+
+// //Input method
+// a=prompt("Enter yout name")
+// document.write(a)
+
+//Decleration 
+let a;
+
+//Initialization
+let b=49;
+var c=56;
+const d=45;
+
+document.writeln(b)
+document.write(c)
