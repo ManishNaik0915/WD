681dbbeae1155aa980959b9948de1a337d1e73be
ManishNaik0915 <manishnaik35980@gmail.com>
2026-09-01 12:09:15 +0530

    Day 12 & 13 JS

--- JS/data.js
+++ JS/data.js
@@ -36,3 +36,38 @@ let k=a.slice(3)
 console.log(k);
 
 
+//Day 13
+
+//Number
+let x=456.3452187
+console.log(x)
+console.log(typeof(x));
+
+let y=x.toString()
+console.log(y);
+console.log(typeof(y));
+
+let z=x.toFixed(2)
+console.log(y);
+
+
+//Boolean
+let n=true
+console.log(n);
+console.log(typeof(n));
+
+
+//NULL
+let m=null
+console.log(m);
+console.log(typeof(m));
+
+
+//Undefined
+let p;
+console.log(p);
+console.log(typeof(p));
+
+let q=67
+let r="apple"
+console.log(q/r);
---
+++ JS/function.js
@@ -0,0 +1,103 @@
+//Day
+
+//Named function
+function add(a,b){
+    console.log(a+b);
+}
+add(10,20);
+
+//Ananomous Function
+(function (a,b){
+    console.log(a+b);
+})(10,20);
+
+//IIF
+(function add(a,b){
+    console.log(a+b);
+}
+)(20,20);
+
+//Function with expression
+let sub=function(a,b){
+    console.log(a-b);
+}
+sub(4,6)
+
+//Arrow Function
+let div=((a,b)=>{
+    console.log(a/b);
+})
+div(30,60)
+
+//Nested Function
+function grandparent(){
+    console.log("I am GP");
+        function parent(){
+            console.log("I am Parent");
+                function child(){
+                    console.log("I am Child");
+                }
+                // child()
+                return child
+        }
+        // parent()
+        return parent
+}
+// grandparent()
+grandparent()()()
+
+let d=56;   //global
+function mul(){
+    let a=30;
+    letb=20;
+    let c=a*b
+    console.log(d);
+    return c;
+}
+
+
+//Object
+//key:value;
+//Literals;
+let emp={
+    Id:101,
+    Name:"Manish",
+    City:"Pune"
+}
+console.log(emp);
+console.table(emp);
+console.log(Object.keys(emp));
+console.log(Object.values(emp));
+
+
+
+//Constructor function
+let emp1={
+    Id:101,
+    Name:"Manish",
+    Address:{
+        Area:"ShivajiNagar",
+        City:"Pune"
+    }
+}
+console.log(emp1);
+console.table(emp1);
+console.log(Object.keys(emp1));
+console.log(Object.values(emp1));
+
+let emp2={
+    Id:102,
+    Name:"Manish",
+    Address:{
+        Area:"ShivajiNagar",
+        City:"Pune"
+    }
+}
+console.log(emp2);
+console.table(emp2);
+console.log(Object.keys(emp2));
+console.log(Object.values(emp2));
+
+
+console.table([emp1,emp2]);
+
