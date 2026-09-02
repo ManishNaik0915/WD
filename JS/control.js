37c0a7f5260295aa2b420209879e72ed002970c4
ManishNaik0915 <manishnaik35980@gmail.com>
2026-09-02 11:51:05 +0530

    Day 14 JS

---
+++ JS/control.js
@@ -0,0 +1,19 @@
+//Day 14
+
+let num=prompt("Enter number")
+if(num%2==0){
+    console.log("Even Number")
+}
+else{
+    console.log("Odd Number")
+}
+
+
+
+let int1=prompt("Enter Number")
+if(int1>=0){
+    console.log("Positive")
+}
+else{
+    console.log("Negitive")
+}
