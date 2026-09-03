d19d5de88b0dca484fed8c2b5d70b58a6b0becc8
ManishNaik0915 <manishnaik35980@gmail.com>
2026-09-03 12:03:32 +0530

    Day 14&15 JS

--- JS/control.js
+++ JS/control.js
@@ -1,19 +1,128 @@
 //Day 14
 
-let num=prompt("Enter number")
-if(num%2==0){
-    console.log("Even Number")
-}
-else{
-    console.log("Odd Number")
-}
+
+// //ood Even Number
+// let num=prompt("Enter number")
+// if(num%2==0){
+//     console.log("Even Number")
+// }
+// else{
+//     console.log("Odd Number")
+// }
+
+
+// //Positive Negitive Number
+// let int1=prompt("Enter Number")
+// if(int1>=0){
+//     console.log("Positive")
+// }
+// else{
+//     console.log("Negitive")
+// }
 
 
 
-let int1=prompt("Enter Number")
-if(int1>=0){
-    console.log("Positive")
+//Day 15
+
+
+// //else if Statement
+// let mark=prompt("Enter Marks")
+// if (mark==65){
+//     console.log("You are Eligible for Java session");
+// }
+// else if (mark==75){
+//     console.log("You are eligible for Java with AI session ");
+// }
+// else if (mark>=76){
+//     console.log("You are eligible for Developer Course ");
+// }
+// else{
+//     console.log("Keep Studying");
+// }
+
+
+// //Switch case
+
+// let time=7;
+// switch (time){
+//     case 7:
+//         console.log("This is Morning");
+//     break;
+
+//     case 12:
+//         console.log("This is Afternoon");
+//     break;
+
+//     case 4:
+//         console.log("This is Evening");
+//     break;
+
+//     case 10:
+//         console.log("This is Night");
+//     break;
+    
+//     default:
+//         console.log("Invalid Input");
+// }
+
+// let Signal=prompt("Enter Signal");
+// switch (Signal){
+//     case "Red":
+//         console.log("Stop")
+//     break;
+
+//     case "Green":
+//         console.log("Go");
+//     break;
+
+//     case "Yellow":
+//         console.log("Be ready");
+//     break;
+
+//     default:
+//         console.log("Invalid input");
+
+// }
+
+
+// //Looping Statements
+// // 1.for loop
+// // syntax: for(inr;cond;inc/dec)
+// //            block of code
+// //         }
+
+// let a;
+// for(a=1;a<=50;a++){
+//     console.log(a);
+
+//     if(a%2==0){
+//         console.log(a);
+//     }
+// }
+
+
+// //2.While loop
+// let num=1; 
+// while (num<=50){
+//     if(num%2==0){
+//         console.log(num)
+//     }
+//     console.log(num);
+//     num++;
+// }
+
+
+// //3.Do while loop
+// Syntax:
+//     initialization
+//     do
+//     block of Code 
+//     inc/dec;
+//     }
+//     while(condition)
+let i=1;
+do{
+    console.log(i);
+    i++
 }
-else{
-    console.log("Negitive")
-}
+while(i<=50)
